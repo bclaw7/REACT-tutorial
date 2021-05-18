1cd676a58d65299ecd33d8dfede1b383c0724b77
bclaw <bclaw8300@gmail.com>
2021-05-18 00:06:48 -0600

    added gitignore

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,34 +1,36 @@
 import logo from './logo.svg';
-import './App.css';
-import Greet from './components/Greet'
-import Welcome from './components/Welcome'
-import Hello from './components/Hello'
-import Message from './components/Message'
-import Counter from './components/Counter'
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
-
+import React, { useState } from "react";
+import "./App.css";
+import Greet from "./components/Greet";
+import Welcome from "./components/Welcome";
+import Hello from "./components/Hello";
+import Message from "./components/Message";
+import Counter from "./components/Counter";
+import FunctionClick from "./components/FunctionClick";
+import ClassClick from "./components/ClassClick";
+import Circle from './components/ThanksClick';
 
 function App() {
-  return (
-    <div className="App">
-        <ClassClick />
-        <FunctionClick />
-        <Counter />
-        <Message />
-      <Greet name="Bruce" heroName="Batman">
-          <p>This is children props</p>
-      </Greet>
-      <Greet name="Anna" heroName="Wonder Woman">
-          <button>Action</button>
-      </Greet>
-      <Greet name="Clark" heroName="Superman" />
-      <Welcome name="Bruce" heroName="Batman"/>
-      <Welcome name="Anna" heroName="Wonder Woman"/>
-      <Welcome name="Clark" heroName="Superman"/>
-      <Hello />
-    </div>
-  );
+	return (
+		<div className="App">
+            <ThanksClick />;
+			<ClassClick />;
+			<FunctionClick />
+			<Counter />
+			<Message />
+			<Greet name="Bruce" heroName="Batman">
+				<p>This is children props</p>
+			</Greet>
+			<Greet name="Anna" heroName="Wonder Woman">
+				<button>Action</button>
+			</Greet>
+			<Greet name="Clark" heroName="Superman" />
+			<Welcome name="Bruce" heroName="Batman" />
+			<Welcome name="Anna" heroName="Wonder Woman" />
+			<Welcome name="Clark" heroName="Superman" />
+			<Hello />
+		</div>
+	);
 }
 
 export default App;
---
+++ hello-world/src/components/ThanksClick.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ThanksClick() {
+    function clickHandler ()
+    return (
+        <div>
+            <input type="text" />
+        </div>
+    )
+}
+
+export default Circle
